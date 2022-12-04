7c07f668a722132eb02c47a6180ed63272d2abf6
Vladyslav <vlad.dalechin@gmail.com>
2022-12-04 12:16:21 +0200

    feat: dirty signatures

--- src/VRFSignatured.sol
+++ src/VRFSignatured.sol
@@ -5,6 +5,11 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 abstract contract VRFSignatured {
   address public immutable vrfSigner;
+  mapping (bytes => bool) public dirty;
+
+  error DirtySignature();
+  error InvalidSignature();
+
   constructor(address _vrfSigner) {
     vrfSigner = _vrfSigner;
   }
@@ -12,8 +17,12 @@ abstract contract VRFSignatured {
   function verifyRandomness(
     uint256 _randomness,
     bytes memory _signature
-  ) public view returns (bool) {
+  ) internal {
+    if (dirty[_signature]) {
+      revert DirtySignature();
+    }
+    dirty[_signature] = true;
     bytes32 message = keccak256(abi.encodePacked(msg.sender, _randomness));
-    return ECDSA.recover(ECDSA.toEthSignedMessageHash(message), _signature) == vrfSigner;
+    if (ECDSA.recover(ECDSA.toEthSignedMessageHash(message), _signature) != vrfSigner) revert InvalidSignature();
   }
 }
--- test/VRFSignatured.t.sol
+++ test/VRFSignatured.t.sol
@@ -9,7 +9,7 @@ contract VRFLottery is VRFSignatured {
     constructor(address _vrfSigner) payable VRFSignatured(_vrfSigner) {}
 
     function claim(uint256 _randomness, bytes memory _signature) public {
-        require(verifyRandomness(_randomness, _signature), "invalid signature");
+        verifyRandomness(_randomness, _signature);
         // sends all balance to the sender
         payable(msg.sender).transfer(address(this).balance);
     }
@@ -35,5 +35,29 @@ contract VRFSignaturedTest is Test {
 
         assertEq(payable(address(lottery)).balance, 0);
         assertEq(payable(BOB).balance, 1 ether);
+        vm.stopPrank();
+    }
+
+    function testCannotVerifyRandomnessTwice() public {
+        // signed keccak256(BOB, 1337) by ALICE
+        bytes memory signature = hex"dcc3b0537e3a80e9f7fc2343ce8db13bdebabb8472190a7fe2b8978e83f9d5011bdfc1ec502201f0d4c3f9c9f64ead60ca80b4d6f0ea4e477ff858080c0756361c";
+
+        vm.startPrank(BOB);
+        lottery.claim(1337, signature);
+        vm.stopPrank();
+
+        vm.startPrank(BOB);
+        vm.expectRevert(abi.encodeWithSignature("DirtySignature()"));
+        lottery.claim(1337, signature);
+        vm.stopPrank();
+    }
+
+    function testCannotVerifyInvalidSIgnature() public {
+        bytes memory signature = hex"deadbeef";
+
+        vm.startPrank(BOB);
+        vm.expectRevert(abi.encodeWithSignature("InvalidSignature()"));
+        lottery.claim(1337, signature);
+        vm.stopPrank(); 
     }
 }
